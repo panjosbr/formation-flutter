83d9f64fee6238051fbede54b6f22273e40c0af3
Paulo dos Anjos <paulopereiradosanjos@gmail.com>
2023-02-08 12:13:22 -0300

    Criando class ExampleWidget para guardar exemplos feitos em aula

---
+++ flutter_widgets_states/lib/examples.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class ExampleWidget {
+  Widget _elevatedButton() {
+    return ElevatedButton(onPressed: () {}, child: Text('Salvar'));
+  }
+
+  Widget text(String text, Color color) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 20,
+        color: color,
+        decoration: TextDecoration.none,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  Widget screen() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        stack(),
+        stack(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              color: Colors.cyan,
+              width: 50,
+              height: 50,
+            ),
+            Container(
+              color: Colors.blueAccent,
+              width: 50,
+              height: 50,
+            ),
+          ],
+        ),
+        _elevatedButton(),
+      ],
+    );
+  }
+
+  Widget row() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+        ),
+        Container(
+          color: Colors.blueAccent,
+          width: 50,
+          height: 50,
+        ),
+      ],
+    );
+  }
+
+  Widget col() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+        ),
+        Container(
+          color: Colors.blueAccent,
+          width: 50,
+          height: 50,
+        ),
+      ],
+    );
+  }
+
+  Widget stack() {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+        ),
+        Container(
+          color: Colors.blueAccent,
+          width: 50,
+          height: 50,
+        ),
+      ],
+    );
+  }
+}
