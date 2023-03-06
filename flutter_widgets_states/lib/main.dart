0bdb5f23abd95c4b71059db396ea5410fbeec234
Paulo dos Anjos <paulopereiradosanjos@gmail.com>
2023-03-06 14:47:33 -0300

    adicionando animacao para ocultar tarefas

--- flutter_widgets_states/lib/main.dart
+++ flutter_widgets_states/lib/main.dart
@@ -3,11 +3,18 @@ import 'package:flutter/material.dart';
 import 'widgets/task.widget.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacity = true;
 
   // This widget is the root of your application.
   @override
@@ -22,24 +29,36 @@ class MyApp extends StatelessWidget {
           leading: Container(),
           title: const Text('Tarefas'),
         ),
-        body: ListView(
-          children: [
-            TaskWidget(
-              'Aprender Flutter Aprender',
-              'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-              5,
-            ),
-            TaskWidget(
-              'Ler',
-              'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-              3,
-            ),
-            TaskWidget(
-              'Jogar',
-              'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
-              1,
-            ),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacity ? 1 : 0,
+          duration: Duration(milliseconds: 1000),
+          child: ListView(
+            children: [
+              TaskWidget(
+                'Aprender Flutter Aprender',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
+                5,
+              ),
+              TaskWidget(
+                'Ler',
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
+                3,
+              ),
+              TaskWidget(
+                'Jogar',
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg',
+                1,
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacity = !opacity;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
         ),
       ),
     );
