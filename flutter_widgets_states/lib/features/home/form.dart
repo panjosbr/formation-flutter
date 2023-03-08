00a17fcee23e8c65406d691783704c4307d650da
Paulo dos Anjos <paulopereiradosanjos@gmail.com>
2023-03-08 13:43:27 -0300

    Tratando imagem invalida com errorBuilder

--- flutter_widgets_states/lib/features/home/form.dart
+++ flutter_widgets_states/lib/features/home/form.dart
@@ -30,6 +30,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -91,6 +93,13 @@ class _FormScreenState extends State<FormScreen> {
                     child: Image.network(
                       imageController.text,
                       fit: BoxFit.cover,
+                      errorBuilder: (
+                        BuildContext context,
+                        Object exception,
+                        StackTrace? stackTrace,
+                      ) {
+                        return Image.asset('lib/assets/images/no-image.jpg');
+                      },
                     ),
                   ),
                 ),
