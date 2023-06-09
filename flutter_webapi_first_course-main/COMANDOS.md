57dfefd752951b7635207916565264f9f43ce450
Paulo dos Anjos <paulopereiradosanjos@gmail.com>
2023-06-09 09:59:51 -0300

    Adicionando journal Service usando http e http-interceptor

---
+++ flutter_webapi_first_course-main/COMANDOS.md
@@ -0,0 +1,5 @@
+### rodar json-server apontando pra um json
+json-server --watch --host 192.168.0.65 db.json
+
+#no powershell - desabilitar politicas que atrapalha o json-server
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
--- flutter_webapi_first_course-main/lib/main.dart
+++ flutter_webapi_first_course-main/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webapi_first_course/services/journal_service.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
+
+  JournalService service = JournalService();
+  service.register('Hello World!');
+  // service.get();
 }
 
 class MyApp extends StatelessWidget {
@@ -12,6 +18,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        appBarTheme: const AppBarTheme(
+          color: Colors.black38,
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        textTheme: GoogleFonts.hammersmithOneTextTheme(),
+      ),
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       initialRoute: "home",
