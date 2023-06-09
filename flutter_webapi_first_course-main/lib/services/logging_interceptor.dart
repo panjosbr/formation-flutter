1f095f9661c23f26144cf92eb839348cf1df56c5
Paulo dos Anjos <paulopereiradosanjos@gmail.com>
2023-06-09 10:12:17 -0300

    melhorando logs com Logger

--- flutter_webapi_first_course-main/lib/services/logging_interceptor.dart
+++ flutter_webapi_first_course-main/lib/services/logging_interceptor.dart
@@ -1,15 +1,29 @@
 import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
 
 class LoggingInterceptor implements InterceptorContract {
+  Logger logger = Logger();
+
   @override
   Future<RequestData> interceptRequest({required RequestData data}) async {
+    logger.v(
+      'Requisição para ${data.baseUrl} \nCabeçalhos: ${data.headers} \nBody: ${data.body}',
+    );
     print(data.toString());
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({required ResponseData data}) async {
-    print(data.toString());
+    if (data.statusCode ~/ 100 == 2) {
+      logger.i(
+        'Resposta de ${data.url} \nStatus da resposta: ${data.statusCode}\nHeader: ${data.headers} \nBody: ${data.body}',
+      );
+    } else {
+      logger.e(
+        'Resposta de ${data.url} \nStatus da resposta: ${data.statusCode} \nHeader: ${data.headers} \nBody: ${data.body}',
+      );
+    }
     return data;
   }
 }
